968c304e071a26265023ba423f55d5b2b91104f3
nthuTYChen <31963114+nthuTYChen@users.noreply.github.com>
2017-11-01 10:48:44 +0800

    Update Chinese Comments

--- client/main.js
+++ client/main.js
@@ -4,40 +4,66 @@
     Last Updated on Nov 1, 2017
 */
 
-msgRecords = new Mongo.Collection('msgRecords');
-
-Meteor.startup(function(){
-  //Actions executed after the app has started.
-});
+//把msgRecords的mongoDB資料庫連結到msgRecords這個用戶端的Global Variable
+msgRecords = new Mongo.Collection(msgRecords); //請勿變更此行
 
 Template.body.helpers({
-  'allMsg': function() {
-    var allMsgs = msgRecords.find({}, {$sort: {createdAt: -1}}).fetch(), msgTexts = '';
+  allMsg: function() {
+    //從msgRecords搜尋所有訊息，並且依createdAt的內容以遞減方式排序
+    //因為資料庫的存取是有反應性(reactive)的，每次資料庫的內容變更
+    //整個helpers就會重新執行一次，所以網頁的內容就會隨資料庫內容改變
+    var allMsgs = msgRecords.find({}, {$sort: {createdAt: -1}});
+    //將資料庫搜尋結果轉換為一個物件陣列
+    allMsgs = allMsgs.fetch();
+    //建立一個變數儲存要回傳的所有訊息字串
+    var msgTexts = "";
+    //檢查是不是有從資料庫找到任何訊息。有找到任何訊息的話allMsgs物件陣列的
+    //長度會大於0
     if(allMsgs.length > 0)
     {
-      for(i=0 ; i<allMsgs.length; i++)
+      //利用for迴圈合併每個訊息中的說話者跟訊息內容並儲存到msgTexts裡
+      for(index=0 ; i<allMsgs.length; index++)
       {
-        msgTexts += allMsgs[i].speaker+': '+allMsgs[i].msg+'\n';
+        //allMsgs[index].speaker = 這筆訊息的說話者
+        //allMsgs[index].msg = 這筆訊息的內容
+        //在最後加上"\n"這個代表換行的字串
+        msgTexts = msgTexts+
+                    allMsgs[index].speaker+': '+allMsgs[index].msg+'\n';
       }
     }
+    //在allMsgs陣列長度不是大於0的時候，代表資料庫中沒有任何訊息
     else
     {
-      msgTexts = 'ELIZA: This is ELIZA. How are you doing today?';
+      //這時候將一筆預設訊息儲存到msgTexts裡
+      msgTexts = "ELIZA: This is ELIZA. How are you doing today?";
     }
+    //回傳msgTexts
     return msgTexts;
   }
 });
 
+//定義網頁事件
 Template.body.events({
-  'click #submitMsg': function(event) {
+  //事件：當網頁中id=submitMsg的物件被點選的時候
+  "click #submitMsg": function(event) {
+    //每個事件的功能函數function()都可以接受一個事件的變數，這邊我們取名做event
+    //event.preventDefault()代表這個事件我們不要執行預設的HTML功能，避免每次按
+    //Submit的按鈕都會重新讀取網頁
     event.preventDefault();
-    var myMsg = document.getElementById('myMsg').value;
-    document.getElementById('myMsg').value = '';
-
-    Meteor.call('msgReceiver', myMsg);
+    //建立一個變數myMsg儲存輸入在網頁中id=myMsg物件的內容(value)
+    var myMsg = document.getElementById("myMsg").value;
+    //把網頁中id=myMsg的內容設定為一個空的字串
+    document.getElementById("myMsg").value = "";
+    //呼叫大腦的msgReceiver功能，並且傳送myMsg變數
+    Meteor.call("msgReceiver", myMsg);
   },
-  'click #resetMsg': function(event) {
+  //事件：當網頁中id=resetMsg的物件被點選的時候
+  "click #resetMsg": function(event) {
+    //同上，避免Reset按鈕執行預設的HTML功能
     event.preventDefault();
-    Meteor.call('resetELIZA');
+    //把網頁中id=myMsg的內容設定為一個空的字串
+    document.getElementById("myMsg").value = "";
+    //呼叫大腦的resetELIZA功能，但不傳送任何訊息
+    Meteor.call("resetELIZA");
   }
 });
--- server/main.js
+++ server/main.js
@@ -4,33 +4,50 @@
     Last Updated on Nov 1, 2017
 */
 
-msgRecords = new Mongo.Collection('msgRecords'); //Don't change this line
+//把msgRecords的mongoDB資料庫連結到msgRecords這個伺服器端的Global Variable
+msgRecords = new Mongo.Collection("msgRecords"); //請勿變更此行
 
-//All server functions called on the client side go to this section
+Meteor.startup(function(){
+  //所有在程式啟動時會在伺服器執行的程式碼都會放在這裡
+});
+
+//所有大腦(伺服器)的功能都會在這裡定義
 Meteor.methods({
-  msgReceiver: function(msg) { //Don't change this line
+  //接收訊息的大腦功能msgReceiver，會接收到一個訊息msg。請勿變更此功能內容
+  msgReceiver: function(msg) {
 
-    //Each messege record inserted into the msgRecords database must have
-    //three fields: 'createdAt' (specify the time being inserted), 'speaker'
-    //(the message owner), and 'text' (the message itself)
-    msgRecords.insert({createdAt: new Date(), speaker: 'You', msg: msg}); //Don't change this line
+    //每一個訊息都會被放進msgRecords資料庫，而每一筆資料都會包含三種資訊：
+    //createdAt代表訊息被存入資料庫的時間(new Date()會得到當下的系統時間)、speaker
+    //代表說話者(接收到的訊息的說話者是You)、msg則是訊息本身(接收到的msg變數)
+    msgRecords.insert({createdAt: new Date(), speaker: "You", msg: msg}); //請勿變更此行
 
     processMsg(msg);
-
-    return; //Don't change this line
-  },        //Don't change this line
+    //回傳一個執行完畢的訊號
+    return;
+  },
+  //重設訊息資料庫的大腦功能resetELIZA，除預設訊息的內容之外，請勿變更此功能
   resetELIZA: function() {
+    //移除所有msgRecords資料庫的記憶
     msgRecords.remove({});
-    msgRecords.insert({createdAt: new Date(), speaker: 'ELIZA', msg: 'This is ELIZA. How are you doing today?'});
-
+    //移除所有資料後放入一筆預設的訊息資料
+    msgRecords.insert({createdAt: new Date(), speaker: "ELIZA", msg: "This is ELIZA. How are you doing today?"});
+    //回傳一個執行完畢的訊號
     return;
   }
 });
 
-processMsg = function(msg) {
-  var processResults = '';
-  //Edit your core ELIZA processing function below
-  processResults = 'Hello world!';
-  //Edit your core ELIZA processing function above
+//自訂的大腦功能函數，只能在伺服器內部呼叫。在這邊是由msgReceiver這個大腦功能呼叫
+//會接收到一個msg訊息並進行訊息運算與處理
+var processMsg = function(msg) {  //請勿變更此行
+  //建立一個processResults變數儲存訊息運算處理的結果
+  var processResults = "";  //請勿變更此行
+  //「以下」是你可以編輯的部份，請將你的ELIZA處理訊息的核心程式碼放在以下的段落內
+
+  //目前的訊息處理很簡單，不管收到的msg訊息是什麼，運算結果都只是Hello World
+  processResults = "Hello world!";
+
+  //「以上」是你可以編輯的部份，請將你的ELIZA處理訊息的核心程式碼放在以下的段落內
+
+  //在msgRecords資料庫放入運算訊息之後的結果，做為ELIZA的回應，請勿變更此行
   msgRecords.insert({createdAt: new Date(), speaker: 'ELIZA', msg: processResults});
-};
+};//請勿變更此行
