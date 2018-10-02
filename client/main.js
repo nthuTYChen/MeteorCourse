9b1191988d8f902766da94373c24f65211624c16
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-02 18:22:16 +0800

    Local vs Global Variables

--- client/main.js
+++ client/main.js
@@ -1,18 +1,14 @@
-// This a note
-//var 1dozen = 12;
-var 蘋果數量 = 3;
-/*var helloMessage = "Hello World!";
-console.log(helloMessage);
-helloMessage = 3;
-console.log(helloMessage);*/
-
-/*var fruit = "apple", appleNum = 3;
-console.log(fruit);
-console.log(appleNum);*/
-
 /*
     ELIZA Meteor Template Created by CHEN, Tsung-Ying
     for the NTHU course "Basic Web Linguistic Application Development"
     Last Updated on Oct 2, 2018
 */
+
+var globalVar = "I'm everywhere!";
+
+{
+  let localVar = "I'm invisible somewhere!";  
+  console.log(localVar);
+}
+console.log(globalVar);
 
