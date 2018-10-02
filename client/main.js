aa6ac2dafe4ab2719bfc776005207431f5b66a06
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-02 17:04:01 +0800

    Variable Introduction

--- client/main.js
+++ client/main.js
@@ -1,6 +1,14 @@
 // This a note
-var helloMessage = "Hello World!";
+//var 1dozen = 12;
+var 蘋果數量 = 3;
+/*var helloMessage = "Hello World!";
 console.log(helloMessage);
+helloMessage = 3;
+console.log(helloMessage);*/
+
+/*var fruit = "apple", appleNum = 3;
+console.log(fruit);
+console.log(appleNum);*/
 
 /*
     ELIZA Meteor Template Created by CHEN, Tsung-Ying
