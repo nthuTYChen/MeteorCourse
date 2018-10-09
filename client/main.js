654fc6143a5f35e27ce01e095012bf3786120eae
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-09 18:15:59 +0800

    Week 5 Array and Object

--- client/main.js
+++ client/main.js
@@ -4,7 +4,39 @@
     Last Updated on Oct 2, 2018
 */
 
-var name1 = "Jessica", name2 = "Jason", name3 = "Tom";
+var Mike = {
+  age: 28,
+  gender: "Male",
+  height: 180,
+  weight: 79
+};
+
+Mike["age"] = 30;
+console.log(Mike["age"]);
+
+/*var numbers = [10, 20, 30, 40, 50];
+//              0   1   2   3   4
+var emptyArray = [];
+
+var Mike = [28, "Male", 180, 79, "Married"];
+
+var names = ["Mike", "John", "Mary", "Tom"];
+var ages = [27, 18, 65, 9];
+
+for(let index = 0 ; index < 5 ; index++) {
+  console.log(names[index]);
+  console.log(ages[index]);
+}*/
+
+/*var arrayIndex = 3;
+console.log(numbers[arrayIndex]);*/
+
+/*console.log(numbers[0]);
+console.log(numbers[2]);
+numbers[0] = 100;
+console.log(numbers);*/
+
+/*var name1 = "Jessica", name2 = "Jason", name3 = "Tom";
 var msg1 = "How are you?", msg2 = "Hey, man!", msg3 = "Blah!";
 var age1 = 18, age2 = 40, age3 = 22;
 
@@ -32,7 +64,7 @@ var ageComment = function(name, age) {
 
 ageComment(name1, age1);
 ageComment(name2, age2);
-ageComment(name3, age3);
+ageComment(name3, age3);*/
 
 /*var adding = function(x, y) {
   let result = x + y;
