d7f485be44462ad8f7f9220a050226bbb204a7b9
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-09 17:28:25 +0800

    Week 4 Practice 2

--- client/main.js
+++ client/main.js
@@ -4,7 +4,49 @@
     Last Updated on Oct 2, 2018
 */
 
+var name1 = "Jessica", name2 = "Jason", name3 = "Tom";
+var msg1 = "How are you?", msg2 = "Hey, man!", msg3 = "Blah!";
+var age1 = 18, age2 = 40, age3 = 22;
 
+var response = function(name, msg) {
+  if(msg === "How are you?") {
+    console.log("Good. How are you, "+name);
+  }
+  else {
+    console.log("Go away, "+name);
+  }
+};
+
+response(name1, msg1);
+response(name2, msg2);
+response(name3, msg3);
+
+var ageComment = function(name, age) {
+  if(age < 20) {
+    console.log(name+", you're so young!");
+  }
+  else {
+    console.log(name+", you're an adult!");
+  }
+};
+
+ageComment(name1, age1);
+ageComment(name2, age2);
+ageComment(name3, age3);
+
+/*var adding = function(x, y) {
+  let result = x + y;
+  return result;
+};
+
+var finalResult = adding(3, 5);
+console.log("The result is "+finalResult);*/
+
+/*var sayHi = function() {
+  console.log("Hello!");
+};
+
+sayHi();*/
 
 /*var result = 0;
 
