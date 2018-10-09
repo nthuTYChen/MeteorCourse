d3884c657b8d143dd9a186f2cb27817ffae8dbdf
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-09 16:14:48 +0800

    Week 5 Course Demo 1

--- client/main.js
+++ client/main.js
@@ -4,11 +4,36 @@
     Last Updated on Oct 2, 2018
 */
 
-var globalVar = "I'm everywhere!";
+for(let age=0 ; age < 100 ; age++) {
+  if(age === 0) {
+    console.log(age+" : Welcome to the world!");
+  }
+  else if(age === 25) {
+    console.log(age+" : You're so energetic!");
+  }
+  else if(age === 75) {
+    console.log(age+" : You've changed a lot.");
+  }
+  else if(age === 99) {
+    console.log(age+" : Your time has come."); //QQ
+  }
+}
 
-{
+/*var x = 5, y = 10;
+
+if(x > y) {
+  let result = x * y;
+  console.log(result);
+}
+else {
+  let result = x / y;
+  console.log(result);
+}*/
+
+/*{
   let localVar = "I'm invisible somewhere!";  
   console.log(localVar);
+  var globalVar = "I'm everywhere!";
 }
-console.log(globalVar);
+console.log(globalVar);*/
 
