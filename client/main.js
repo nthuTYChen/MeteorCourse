70c0e6a66603d4f90a855790842a49b9d2fb4a9f
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-16 15:01:58 +0800

    Week 6
    Clean up old examples

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 /*
     ELIZA Meteor Template Created by CHEN, Tsung-Ying
     for the NTHU course "Basic Web Linguistic Application Development"
-    Last Updated on Oct 2, 2018
+    Last Updated on Oct 16, 2018
 */
 
 var Mike = {
@@ -13,111 +13,4 @@ var Mike = {
 
 Mike["age"] = 30;
 console.log(Mike["age"]);
-
-/*var numbers = [10, 20, 30, 40, 50];
-//              0   1   2   3   4
-var emptyArray = [];
-
-var Mike = [28, "Male", 180, 79, "Married"];
-
-var names = ["Mike", "John", "Mary", "Tom"];
-var ages = [27, 18, 65, 9];
-
-for(let index = 0 ; index < 5 ; index++) {
-  console.log(names[index]);
-  console.log(ages[index]);
-}*/
-
-/*var arrayIndex = 3;
-console.log(numbers[arrayIndex]);*/
-
-/*console.log(numbers[0]);
-console.log(numbers[2]);
-numbers[0] = 100;
-console.log(numbers);*/
-
-/*var name1 = "Jessica", name2 = "Jason", name3 = "Tom";
-var msg1 = "How are you?", msg2 = "Hey, man!", msg3 = "Blah!";
-var age1 = 18, age2 = 40, age3 = 22;
-
-var response = function(name, msg) {
-  if(msg === "How are you?") {
-    console.log("Good. How are you, "+name);
-  }
-  else {
-    console.log("Go away, "+name);
-  }
-};
-
-response(name1, msg1);
-response(name2, msg2);
-response(name3, msg3);
-
-var ageComment = function(name, age) {
-  if(age < 20) {
-    console.log(name+", you're so young!");
-  }
-  else {
-    console.log(name+", you're an adult!");
-  }
-};
-
-ageComment(name1, age1);
-ageComment(name2, age2);
-ageComment(name3, age3);*/
-
-/*var adding = function(x, y) {
-  let result = x + y;
-  return result;
-};
-
-var finalResult = adding(3, 5);
-console.log("The result is "+finalResult);*/
-
-/*var sayHi = function() {
-  console.log("Hello!");
-};
-
-sayHi();*/
-
-/*var result = 0;
-
-for(let num=1 ; num <= 100 ; num++) {
-  result = result + num;
-}
-
-console.log(result);*/
-
-/*for(let age=0 ; age < 100 ; age++) {
-  if(age === 0) {
-    console.log(age+" : Welcome to the world!");
-  }
-  else if(age === 25) {
-    console.log(age+" : You're so energetic!");
-  }
-  else if(age === 75) {
-    console.log(age+" : You've changed a lot.");
-  }
-  else if(age === 99) {
-    console.log(age+" : Your time has come."); //QQ
-  }
-}*/
-
-/*var x = 5, y = 10;
-
-if(x > y) {
-  let result = x * y;
-  console.log(result);
-}
-else {
-  let result = x / y;
-  console.log(result);
-}*/
-
-/*{
-  let localVar = "I'm invisible somewhere!";  
-  console.log(localVar);
-  var globalVar = "I'm everywhere!";
-}
-console.log(globalVar);*/
 
