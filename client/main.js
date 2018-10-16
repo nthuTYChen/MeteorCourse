68a99c7423c28a17d4ae866ad98959f8441150f0
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-16 16:34:34 +0800

    Week 6 more function demos

--- client/main.js
+++ client/main.js
@@ -4,7 +4,32 @@
     Last Updated on Oct 16, 2018
 */
 
-var Mike = {
+/*var students = ["John", "Mary", "Tim", "David", "Tony", "Amy",
+                "Jim", "Kerry", "Mike", "Stern"];
+
+var rollCall = function() {
+    let randomIndex = Math.random() * 10;
+    randomIndex = Math.floor(randomIndex);
+    let studentName = students[randomIndex];
+    let nameMessage = studentName + " is presenting today!";
+    return nameMessage;
+};
+
+for(let callNum = 1 ; callNum < 3 ; callNum++) {
+    console.log(rollCall());
+}*/
+
+/*var fakeGoogleMap = function(yourPlace, destination) {
+    let fakeDistance = Math.random() * 100;
+    let distMessage = 
+      "The distance between "+yourPlace+" and "+destination+
+      " is "+fakeDistance+" km.";
+    return distMessage;
+};
+
+console.log(fakeGoogleMap("Taipei", "Hsinchu"));*/
+
+/*var Mike = {
   age: 28,
   gender: "Male",
   height: 180,
@@ -12,5 +37,5 @@ var Mike = {
 };
 
 Mike["age"] = 30;
-console.log(Mike["age"]);
+console.log(Mike["age"]);*/
 
