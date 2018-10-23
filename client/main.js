9b00eb767e734eb5463d6e4b211c9504de43551d
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-23 18:19:47 +0800

    Week 7 Templating

--- client/main.js
+++ client/main.js
@@ -4,103 +4,24 @@
     Last Updated on Oct 16, 2018
 */
 
-/*var students = ["John", "Mary", "Tim", "David", "Tony", "Amy",
-                "Jim", "Kerry", "Mike", "Stern"];
+Template.body.onCreated(function() {
+    //alert("Body loaded!");
+});
 
-var rollCall = function() {
-    let randomIndex = Math.random() * 10;
-    randomIndex = Math.floor(randomIndex);
-    let studentName = students[randomIndex];
-    let nameMessage = studentName + " is presenting today!";
-    return nameMessage;
-};
+Template.mainSection.onCreated(function() {
+    //alert("Main section loaded!");
+});
 
-for(let callNum = 1 ; callNum < 3 ; callNum++) {
-    console.log(rollCall());
-}*/
+Template.formSection.events({
+  "click #submitMsg": function(event) {
+    event.preventDefault();
+    //alert("Message submitted!");
+  },
+  "click .buttons": function() {
+    alert("A button is clicked");
+  }
+});
 
-/*var fakeGoogleMap = function(yourPlace, destination) {
-    let fakeDistance = Math.random() * 100;
-    let distMessage = 
-      "The distance between "+yourPlace+" and "+destination+
-      " is "+fakeDistance+" km.";
-    return distMessage;
-};
-
-console.log(fakeGoogleMap("Taipei", "Hsinchu"));*/
-
-/*var myFirstBike = {
-  brand: "GIANT",
-  when: 1993,
-  color: "red",
-  price: 5500,
-  stolenYears: [1995, 2000, 2004]
-};
-
-var stolenYears = myFirstBike.stolenYears;
-
-console.log(stolenYears[0]);*/
-
-/*var Mike = {
-  age: 28,
-  gender: "Male",
-  height: 180,
-  weight: 79
-};
-
-Mike["age"] = 30;
-console.log(Mike["age"]);*/
-
-/*var coffeeMachine = {
-    makeEspresso: function() {
-        console.log("Here's your espresso!");
-    },
-    makeLatte: function() {
-        console.log("Here's your Latte!");
-    },
-    makeAnything: function(request) {
-        console.log("Here's your "+request);
-    }
-};
-
-coffeeMachine.makeAnything("oolong tea");*/
-
-/*var emptyObj = {};
-
-emptyObj.price = 5500;
-console.log(emptyObj.price);
-
-delete emptyObj.price;
-console.log(emptyObj.price)*/
-
-/*var numbers = [1, 2, 3, 4, 5];
-
-for(let index = (numbers.length-1) ; index >= 0 ; index--) {
-    console.log(numbers[index]);
-}*/
-
-/*var numbers = [25, 98, 1, 17, 20, 39];
-var hugeNums = [];
-
-for(let index = 0 ; index < numbers.length ; index++) {
-    let num = numbers[index];
-    if(num > 30) {
-      hugeNums.push(num);
-    }
-    else {
-      console.log(num);
-    }
-}
-
-console.log(hugeNums);*/
-
-var str = "This is a string.";
-
-str = str.replace("string", "message");
-console.log(str);
-
-var words = str.split("is");
-console.log(words);
 
 
 
