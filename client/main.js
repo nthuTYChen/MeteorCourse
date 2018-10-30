6e62d9a20fbd185202e026a6663cfeaa3dab20b3
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-10-30 18:24:48 +0800

    Week 8 Helper

--- client/main.js
+++ client/main.js
@@ -1,24 +1,53 @@
 /*
     ELIZA Meteor Template Created by CHEN, Tsung-Ying
     for the NTHU course "Basic Web Linguistic Application Development"
-    Last Updated on Oct 16, 2018
+    Last Updated on Oct 30, 2018
 */
 
+var stupidResponse = function() {
+  return "I beg your pardon?";
+};
+
 Template.body.onCreated(function() {
-    //alert("Body loaded!");
+
 });
 
-Template.mainSection.onCreated(function() {
-    //alert("Main section loaded!");
+Template.body.onRendered(function() {
+
+});
+
+Template.mainSection.helpers({
+  getData: function() {
+    return "Here's your data!";
+  },
+  testHelper: function() {
+    return "This data is from a helper!";
+  },
+  stringCombine: function(str1, str2) {
+    return str1+" "+str2;
+  },
+  multipler: function(num1, num2) {
+    let product = parseInt(num1) * parseInt(num2);
+    return product;
+  }
 });
 
 Template.formSection.events({
   "click #submitMsg": function(event) {
     event.preventDefault();
-    //alert("Message submitted!");
+    let myMsgObj = document.getElementById("myMsg");
+    let myMsg = myMsgObj.value;
+    let conBoxObj = document.getElementById("conversationBox");
+    let oldConversation = conBoxObj.value;
+    let newConversation = oldConversation+"\n"+"You: "+myMsg;
+    let ELIZAResponse = stupidResponse();
+    newConversation = newConversation+"\n"+"ELIZA: "+ELIZAResponse;
+    conBoxObj.value = newConversation;
+    myMsgObj.value = "";
   },
-  "click .buttons": function() {
-    alert("A button is clicked");
+  "click #resetMsg": function() {
+    let conBoxObj = document.getElementById("conversationBox");
+    conBoxObj.value = "ELIZA: How are you doing?";
   }
 });
 
