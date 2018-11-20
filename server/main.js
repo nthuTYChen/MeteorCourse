700670aa7453404f6ecbda4e1b1c92009d61fc0d
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-11-20 17:26:32 +0800

    Week 12 MongoDB
     Conversation Log

--- server/main.js
+++ server/main.js
@@ -1,66 +1,44 @@
 var profileDataDB = new Mongo.Collection("profileData");
+var conversationLogDB = new Mongo.Collection("conversationLog");
 
 var stupidResponse = function(msg) {
 	return "What is "+msg+"?";
 };
 
 Meteor.startup(function() {
-	profileDataDB.remove({});
-	profileDataDB.insert(
+	//profileDataDB.remove({});
+	conversationLogDB.remove({});
+	let searchResults = conversationLogDB.find();
+	if(searchResults.fetch().length < 1) {
+		conversationLogDB.insert(
 			{
-				name: "Michael Jackson",
-				affiliation: "Heaven",
-				age: 130
+				source: "ELIZA",
+				msg: "How are you doing?",
+				time: new Date()
 			}
 		);
-	profileDataDB.insert(
-			{
-				name: "TY Chen",
-				affiliation: "NTHU",
-				age: 38
-			}
-		);
-	profileDataDB.insert(
-			{
-				name: "My Mom",
-				affiliation: "NTHU",
-				age: 75
-			}
-		);
-	profileDataDB.insert(
-			{
-				name: "Slash",
-				affiliation: "Guns & Roses",
-				age: 56
-			}
-		);
-	profileDataDB.insert(
-			{
-				name: "Ting Ting",
-				affiliation: "Mars",
-				age: 999
-			}
-		);
-	profileDataDB.update(
-		{
-			affiliation: "NTHU",
-			age: {$gt: 65}
-		},
-		{
-			$set: {active: false}
-		}
-	);
-	let searchResults = profileDataDB.find(
-		{
-			affiliation: "NTHU", 
-		 	age: {$gt: 40}			//gte = greater than or equal to = >
-		}							//gt = greater than
-	);
-	console.log(searchResults.fetch());
+	}
+	console.log(conversationLogDB.find().fetch());
 });
 
 Meteor.methods({
 	msgReceiver: function(msg) {
+		conversationLogDB.insert(
+			{
+				source: "You",
+				msg: msg,
+				time: new Date()
+			}
+		);
+		let ELIZAResponse = stupidResponse(msg);
+		conversationLogDB.insert(
+			{
+				source: "ELIZA",
+				msg: ELIZAResponse,
+				time: new Date()
+			}
+		);
+		console.log(conversationLogDB.find().fetch());
 		return stupidResponse(msg);
 	},
 	serverFunc: function(data1, data2) {
