929603eeeec83c4662dfc39b757bae9058e2e74c
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2018-12-18 16:30:30 +0800

    Week 15 Full Weather Info

--- server/main.js
+++ server/main.js
@@ -31,15 +31,26 @@ var weatherInfo = function(msg) {
 		let wtInfoURL = 
 			"http://api.openweathermap.org/data/2.5/weather?APPID="+APIKey+
 			"&q="+cityName+"&units=metric";
+		let wtInfoURL2 = 
+			"http://api.openweathermap.org/data/2.5/forecast?APPID="+APIKey+
+			"&q="+cityName+"&units=metric&cnt=24";
+		let wtData2 = HTTP.get(wtInfoURL2);
+		console.log(wtData2.data.list[0]);
 		try {
 			wtData = HTTP.get(wtInfoURL);
-			wtData = wtData.data.main;
-			let wtResponse = "It's "+wtData.temp+"C.";
-			return wtResponse;
+			let wtDescription = wtData.data.weather[0];
 			//console.log(wtData);
+			let wtMain = wtData.data.main;
+			let wtWind = wtData.data.wind;
+			let wtResponse = "It's "+wtDescription.description+" in "+
+				cityName+", and the temperature is "+wtMain.temp+"C."+
+				" The wind speed is "+wtWind.speed+" km/hr."+
+				" The highest temperature is "+wtMain.temp_max+", "+
+				"and the lowest temperature is "+wtMain.temp_min+"C.";
+			return wtResponse;
 		}
 		catch(error) {
-			return "I don't the city.";
+			return "I don't know the city.";
 		}
 		return "";
 	}
